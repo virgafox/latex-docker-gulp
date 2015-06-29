231128f170faa1735b1fdb4427d3d7f99ad18928
Marco Virgadamo <virga91@gmail.com>
2015-06-29 10:23:59 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp  = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('compile', shell.task([
-  'docker run -v `pwd`:/tmp texlive pdflatex main',
+  'docker run -v `pwd`:/tmp virgafox/texlive pdflatex main',
   'docker rm $(docker ps -a -q -l)'
 ]));
 
